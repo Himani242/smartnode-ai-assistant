632c2ac1cea6b66a2c378031ca9635dd7b2047cb
Himani242 <technicalwriter.smartnode@gmail.com>
2026-03-16 09:54:04 +0530

    Update rag_engine.py

--- rag_engine.py
+++ rag_engine.py
@@ -1,1 +1,38 @@
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
 
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vector_db = Chroma(
+    persist_directory="vector_db",
+    embedding_function=embedding_model
+)
+
+retriever = vector_db.as_retriever(search_kwargs={"k":3})
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash"
+)
+
+def ask_ai(question):
+
+    docs = retriever.invoke(question)
+
+    context = "\n\n".join([doc.page_content for doc in docs])
+
+    prompt = f"""
+    Answer based only on context.
+
+    Context:
+    {context}
+
+    Question:
+    {question}
+    """
+
+    response = llm.invoke(prompt)
+
+    return response.content
