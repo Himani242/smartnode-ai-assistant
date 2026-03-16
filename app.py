cb80b441a4e1b4d750dcb6b17ef734148a00bb95
Himani242 <technicalwriter.smartnode@gmail.com>
2026-03-16 09:53:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+from rag_engine import ask_ai
 
+st.title("Smart Node AI Assistant")
+
+question = st.text_input("Ask a question")
+
+if question:
+    answer = ask_ai(question)
+    st.write(answer)
