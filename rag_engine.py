ce67ea619a60625fa777d5eff480e502dc7f1741
Himani242 <technicalwriter.smartnode@gmail.com>
2026-03-16 16:50:22 +0530

    Update rag_engine.py

--- rag_engine.py
+++ rag_engine.py
@@ -3,18 +3,21 @@ from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+# Load embedding model
 embedding_model = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
 
+# Load vector database
 vector_db = Chroma(
     persist_directory="vector_db",
-    embedding_function=embedding_model
+    embedding_function=embedding_model,
     collection_name="smartnode_docs"
 )
 
-retriever = vector_db.as_retriever(search_kwargs={"k":3})
+retriever = vector_db.as_retriever(search_kwargs={"k":5})
 
+# Gemini model
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0.3
@@ -27,7 +30,7 @@ def ask_ai(question):
     context = "\n\n".join([doc.page_content for doc in docs])
 
     prompt = f"""
-Answer the question using only the context below.
+Answer the question using the context below.
 
 Context:
 {context}
