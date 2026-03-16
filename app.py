b3d1d6aeda494b3581dd0bbd16f6a243e9500c53
Himani242 <technicalwriter.smartnode@gmail.com>
2026-03-16 17:26:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,30 @@ from rag_engine import ask_ai
 
 st.title("Smart Node AI Assistant")
 
-question = st.text_input("Ask a question")
+# Store conversation
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-if question:
-    answer = ask_ai(question)
-    st.write(answer)
+# Display previous messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Chat input
+user_prompt = st.chat_input("Ask something")
+
+if user_prompt:
+
+    # show user message
+    st.session_state.messages.append({"role": "user", "content": user_prompt})
+    with st.chat_message("user"):
+        st.markdown(user_prompt)
+
+    # get AI response
+    answer = ask_ai(user_prompt)
+
+    # show AI message
+    with st.chat_message("assistant"):
+        st.markdown(answer)
+
+    st.session_state.messages.append({"role": "assistant", "content": answer})
