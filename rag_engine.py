7ad47ac8f3530efe0444523bb390666533d6f89c
Himani242 <technicalwriter.smartnode@gmail.com>
2026-03-16 17:31:23 +0530

    Update rag_engine.py

--- rag_engine.py
+++ rag_engine.py
@@ -2,13 +2,18 @@ import os
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage
 
-# Load embedding model
+
+# Load API key
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+# Embedding model
 embedding_model = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
 
-# Load vector database
+# Vector DB
 vector_db = Chroma(
     persist_directory="vector_db",
     embedding_function=embedding_model,
@@ -19,18 +24,25 @@ retriever = vector_db.as_retriever(search_kwargs={"k":5})
 
 # Gemini model
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-1.5-flash",
+    google_api_key=GOOGLE_API_KEY,
     temperature=0.3
 )
+
 
 def ask_ai(question):
 
     docs = retriever.invoke(question)
 
-    context = "\n\n".join([doc.page_content for doc in docs])
+    if not docs:
+        return "No relevant information found."
+
+    context = "\n\n".join([doc.page_content[:800] for doc in docs])
 
     prompt = f"""
-Answer the question using the context below.
+You are an assistant for Smart Node company.
+
+Answer the question based only on the context.
 
 Context:
 {context}
@@ -39,6 +51,6 @@ Question:
 {question}
 """
 
-    response = llm.invoke(prompt)
+    response = llm.invoke([HumanMessage(content=prompt)])
 
     return response.content
